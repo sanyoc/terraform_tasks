8739f6420880da56b35d78edbc32e43a01ab5e32
Alex <sanyoc@gmail.com>
2023-01-17 16:32:26 +0000

    created new security grup for ALB

--- Task_1/sg.tf
+++ Task_1/sg.tf
@@ -28,3 +28,34 @@ resource "aws_security_group" "myVPC-sg" {
 
   tags = var.task-tags
 }
+
+resource "aws_security_group" "alb-sg" {
+  name        = "myVPC-sg"
+  vpc_id      = "${aws_vpc.task-vpc.id}"
+  description = "My VPC Security Group"
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.task-tags
+}
